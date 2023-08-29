9f67f8b2b1c8b02d9c3d41da890009ca309802dd
jungAh.Han <truth.h406@gmail.com>
2023-08-30 01:06:23 +0900

    add: 상품 카드 컴포넌트, 전체 상품 페이지

---
+++ src/app/pages/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Grid } from '@chakra-ui/layout';
+/* eslint-disable import/no-unresolved */
+import ProductCard from '../../components/ProductCard';
+// eslint-disable-next-line import/extensions
+import getAllProducts from '../../lib/graphql/getAllProducts';
+
+// eslint-disable-next-line import/prefer-default-export
+export const getStaticProps = async () => {
+  const { products } = await graphql.request(getAllProducts);
+  return {
+    revalidate: 60,
+    props: {
+      products,
+    },
+  };
+};
+
+export default function Home(props:any) {
+  return (
+    <Grid gridTemplateColumns="repeat(4, 1fr)" gap="5">
+      {props.products.map((product: {
+        id: string,
+      }) => {
+        <ProductCard key={product.id} {...product} />;
+      })}
+    </Grid>
+  );
+}
