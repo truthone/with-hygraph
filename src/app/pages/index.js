1b011cf35289c3190fa93987b2907feaed0737d4
jungAh.Han <truth.h406@gmail.com>
2023-08-30 23:49:39 +0900

    update: config 추가

--- src/app/pages/index.js
+++ src/app/pages/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { Grid } from '@chakra-ui/layout';
-/* eslint-disable import/no-unresolved */
+import { Grid } from '@chakra-ui/react'
 import ProductCard from '../../components/ProductCard';
-// eslint-disable-next-line import/extensions
 import getAllProducts from '../../lib/graphql/getAllProducts';
 
-// eslint-disable-next-line import/prefer-default-export
 export const getStaticProps = async () => {
   const { products } = await graphql.request(getAllProducts);
   return {
@@ -20,9 +16,7 @@ export const getStaticProps = async () => {
 export default function Home(props) {
   return (
     <Grid gridTemplateColumns="repeat(4, 1fr)" gap="5">
-      {props.products.map((product: {
-        id: string,
-      }) => {
+      {props.products.map((product) => {
         <ProductCard key={product.id} {...product} />;
       })}
     </Grid>
